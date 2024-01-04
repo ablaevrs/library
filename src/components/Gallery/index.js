4cc05d83828e9c6dc1fe96c3714fba26d462ec89
rustem <ablaev.r.s@gmail.com>
2024-01-04 13:20:43 +0300

    gallery horizontal

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,42 +1,38 @@
 import { useState } from 'react'
 import s from './index.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
-export default function Gallery({ 
-    images,
-    controls = 'internal' // internal || external
+export default function Gallery({
+    images
 }) {
+
     const [currentImage, setCurrentImage] = useState(0)
 
-    function prevImage () {
-        if (currentImage > 0) {
-            setCurrentImage(currentImage - 1)
-        }
+    function onPrevArrow () {
+        setCurrentImage(currentImage - 1)
     }
 
-    function nextImage () {
-        if (currentImage !== images.length-1) {
-            setCurrentImage(currentImage + 1)
-        }
+    function onNextArrow () {
+        setCurrentImage(currentImage + 1)
     }
 
     return (
-        <div className={s.rootInternal}>
-            { 
+        <div className={s.root}>
+            {
                 currentImage > 0 && (
-                    <div onClick={prevImage} className={controls === 'internal' ? s.prevArrowInternal : s.prevArrowExternal}>
-                        <FontAwesomeIcon icon={faArrowLeft} />
+                    <div className={s.prevArrow} onClick={onPrevArrow}>
+                        <FontAwesomeIcon icon={faChevronLeft} />
                     </div>
-                ) 
+                )
             }
 
             <img className={s.image} src={ images[currentImage] } alt='' />
 
             {
-                currentImage !== images.length-1 && (
-                    <div onClick={nextImage} className={controls === 'internal' ? s.nextArrowInternal : s.nextArrowExternal}>
-                        <FontAwesomeIcon icon={faArrowRight} />
+                currentImage !== images.length - 1 && (
+                    <div className={s.nextArrow} onClick={onNextArrow}>
+                        <FontAwesomeIcon icon={faChevronRight} />
                     </div>
                 )
             }
