b933bd107e3a67ca01b2ee59b4613d365c2263d8
rustem <ablaev.r.s@gmail.com>
2024-01-04 16:34:14 +0300

    gallery: infinity

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Gallery from './components/Gallery';
 function App() {
   const images = [
     'https://img.freepik.com/free-photo/mountains-lake_1398-1150.jpg',
+    'https://i.pinimg.com/736x/d2/aa/56/d2aa5610a05adaf9956f98a824271906--iphone-wallpaper-we-heart-it.jpg',
     'https://img.freepik.com/free-photo/mesmerizing-scenery-of-green-mountains-with-cloudy-sky-surface_181624-27189.jpg',
     'https://img.freepik.com/free-photo/beautiful-tree-in-the-middle-of-a-field-covered-with-grass-with-the-tree-line-in-the-background_181624-29267.jpg',
     'https://img.freepik.com/free-photo/beautiful-shot-of-a-crystal-clear-lake-next-to-a-snowy-mountain-base-during-a-sunny-day_181624-5459.jpg',
@@ -14,7 +15,8 @@ function App() {
     <div className="App">
         <Gallery 
           images={images}
-          vertical={true}
+          infinity={false}
+          vertical={false}
         />
     </div>
   );
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -4,24 +4,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight, faChevronLeft, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 export default function Gallery({
-    images,
-    vertical = false
+    images = [],
+    vertical = false,
+    infinity = false
 }) {
 
     const [currentImage, setCurrentImage] = useState(0)
 
     function onPrevArrow () {
-        setCurrentImage(currentImage - 1)
+        if (infinity && currentImage === 0) {
+            setCurrentImage(images.length - 1)
+        } else {
+            setCurrentImage(currentImage - 1)
+        }
     }
 
     function onNextArrow () {
-        setCurrentImage(currentImage + 1)
+        if (infinity && currentImage === images.length - 1) {
+            setCurrentImage(0)
+        } else {
+            setCurrentImage(currentImage + 1)
+        }
     }
 
     return (
         <div className={s.root}>
             {
-                currentImage > 0 && (
+                (infinity || currentImage > 0) && (
                     <div className={vertical ? s.prevArrowVertical : s.prevArrow} onClick={onPrevArrow}>
                         <FontAwesomeIcon icon={vertical ? faChevronUp : faChevronLeft} />
                     </div>
@@ -31,7 +40,7 @@ export default function Gallery({
             <img className={s.image} src={ images[currentImage] } alt='' />
 
             {
-                currentImage !== images.length - 1 && (
+                (infinity || currentImage !== images.length - 1) && (
                     <div className={vertical ? s.nextArrowVertical : s.nextArrow} onClick={onNextArrow}>
                         <FontAwesomeIcon icon={vertical ? faChevronDown : faChevronRight} />
                     </div>
